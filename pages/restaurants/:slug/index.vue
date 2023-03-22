dd995528040681eace2b1df570fa80684993b8ff
Tim Arendsen <t.j.m.arendsen@gmail.com>
2023-03-22 17:16:00 +0100

    v0.0.13: Added YouTube embeds and website/menu links

--- pages/restaurants/:slug/index.vue
+++ pages/restaurants/:slug/index.vue
@@ -7,6 +7,13 @@
       class="mx-auto rounded shadow max-h-96 max-w-full object-contain mb-8"
     >
 
+    <div class="flex flex-row gap-x-4 my-4" v-if="restaurant.website || restaurant.menu">
+      <div class="text-sm text-gray-600" v-if="restaurant.website"><a :href="restaurant.website" class="underline"
+                                                                      target="_blank">Bekijk website</a></div>
+      <div class="text-sm text-gray-600" v-if="restaurant.menu"><a :href="restaurant.menu" class="underline"
+                                                                   target="_blank">Bekijk menu</a></div>
+    </div>
+
     <h1 class="text-xl font-bold">{{ restaurant.name }}</h1>
     <article class="prose max-w-none" v-html="$md.render(restaurant.content)"></article>
 
@@ -29,6 +36,15 @@
       class="my-8"
       v-else
     />
+
+    <iframe
+      v-if="restaurant.youtube_link" class="w-full h-96"
+      :src="youtubeEmbed" allow="autoplay; encrypted-media" allowfullscreen
+    ></iframe>
+  </div>
+
+  <div v-else-if="restaurantError">
+    <Alert title="Restaurant niet gevonden" content="Er kon geen restaurant worden gevonden met deze titel"></Alert>
   </div>
 
   <div v-else>
@@ -45,7 +61,8 @@ export default {
   data() {
     return {
       restaurant: null,
-      bitterballen: null
+      bitterballen: null,
+      restaurantError: null
     }
   },
   components: {Alert, BitterballenList},
@@ -83,8 +100,16 @@ export default {
           })
           .catch((error) => {
             reject(error);
+            this.restaurantError = error;
           });
       })
+    }
+  },
+  computed: {
+    youtubeEmbed() {
+      const id = this.restaurant.youtube_link.replace('https://www.youtube.com/watch?v=', '');
+
+      return 'https://www.youtube-nocookie.com/embed/' + id;
     }
   },
   created() {
